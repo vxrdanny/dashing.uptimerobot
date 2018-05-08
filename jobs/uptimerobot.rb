ae47945483245e46417884eb2d0ad590848d083d
Danny <itassist1@gmail.com>
2018-05-08 17:13:46 +0100

    API v2 Working
    I don't know any Ruby language, but managed to get this working with APIv2 on my own Smashing dashboard with the above changes. Hopefully this can help someone else out who was stuck in the same situation.
    
    Replace both "u00000-00000000000000000" with your own API from Uptime Robot.

--- jobs/uptimerobot.rb
+++ jobs/uptimerobot.rb
@@ -1,16 +1,16 @@
 require 'uptimerobot'
 
-apiKey = 'u00000-00000000000000000'
+api_key = ENV['u00000-00000000000000000']
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
-  client = UptimeRobot::Client.new(apiKey: apiKey)
+  client = UptimeRobot::Client.new(api_key: 'u00000-00000000000000000')
 
-  raw_monitors = client.getMonitors['monitors']['monitor']
+  raw_monitors = client.getMonitors['monitors']
 
   items = raw_monitors.map { |monitor| 
     { 
-      label: monitor['friendlyname'], 
-      value: if monitor['status'] == '0' then 'Paused' elsif monitor['status'] == '2' then 'Up' elsif monitor['status'] == '9' then 'Down' else 'Unknown' end      
+      label: monitor['friendly_name'], 
+      value: if monitor['status'] == 0 then 'Paused' elsif monitor['status'] == 2 then 'Up' elsif monitor['status'] == 9 then 'Down' else 'Unknown' end      
     }
 
   }
